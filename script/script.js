d05f9f69d26154bfe1425e3e0c7564d59dfce59f
Francesco <cesaranofrancescomain@gmail.com>
2022-11-30 11:36:47 +0100

    Structure

---
+++ script/script.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue ;
+
+createApp({
+    data() {
+        return{
+        
+        }
+    },
+    methods: 
+    {
+        
+        
+    },
+    mounted() { 
+
+        this.loaded = true;           
+    },
+
+        
+}).mount("#app") 
+
